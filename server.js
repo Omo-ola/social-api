645e2d5ae35c2495d5e71f1f09f87265fbeb6aa4
omoola <omoola410@gmail.com>
2025-08-23 10:11:23 +0100

    rectify bug in server file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 // Start server
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
